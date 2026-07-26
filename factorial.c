9c310f32e4ac86b0c6d7c83ecb23fbb18a79d3e2
shreedhar Madev <shreedharnandrekar@gmail.com>
2026-07-26 22:10:14 +0530

    Add factorial calculation program in C
    Add factorial.c program

---
+++ factorial.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int num, i;
+    unsigned long long fact = 1;
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+
+    if (num < 0)
+    {
+        printf("Factorial is not defined for negative numbers.\n");
+    }
+    else
+    {
+        for (i = 1; i <= num; i++)
+        {
+            fact = fact * i;
+        }
+
+        printf("Factorial of %d = %llu\n", num, fact);
+    }
+
+    return 0;
+}
